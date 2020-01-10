a75630392ac1db5c14e5fcc69608a6661a140a10
RJ Adriaansen <adriaansen@eshcc.eur.nl>
2020-01-10 18:41:16 +0100

    Update aligned_dataset.py

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -143,15 +143,16 @@ class AlignedDataset(BaseDataset):
                     soft_border_mask4.append(torch.Tensor(soft_border_mask).unsqueeze(0))
                     item['soft_'+regions[i]+'_mask'] = soft_border_mask4[i]
             for i in range(4):
-                item[regions[i]+'_A'] = A[:,center[i,1]-rhs[i]/2:center[i,1]+rhs[i]/2,center[i,0]-rws[i]/2:center[i,0]+rws[i]/2]
-                item[regions[i]+'_B'] = B[:,center[i,1]-rhs[i]/2:center[i,1]+rhs[i]/2,center[i,0]-rws[i]/2:center[i,0]+rws[i]/2]
+		item[regions[i]+'_A'] = A[:,int(center[i,1]-rhs[i]/2):int(center[i,1]+rhs[i]/2),int(center[i,0]-rws[i]/2):int(center[i,0]+rws[i]/2)]
+		item[regions[i]+'_B'] = B[:,int(center[i,1]-rhs[i]/2):int(center[i,1]+rhs[i]/2),int(center[i,0]-rws[i]/2):int(center[i,0]+rws[i]/2)]
+
                 if self.opt.soft_border:
                     item[regions[i]+'_A'] = item[regions[i]+'_A'] * soft_border_mask4[i].repeat(input_nc/output_nc,1,1)
                     item[regions[i]+'_B'] = item[regions[i]+'_B'] * soft_border_mask4[i]
             
             mask = torch.ones(B.shape) # mask out eyes, nose, mouth
             for i in range(4):
-                mask[:,center[i,1]-rhs[i]/2:center[i,1]+rhs[i]/2,center[i,0]-rws[i]/2:center[i,0]+rws[i]/2] = 0
+                mask[:,int(center[i,1]-rhs[i]/2):int(center[i,1]+rhs[i]/2),int(center[i,0]-rws[i]/2):int(center[i,0]+rws[i]/2)] = 0
             if self.opt.soft_border:
                 imgsize = self.opt.fineSize
                 maskn = mask[0].numpy()
@@ -185,9 +186,11 @@ class AlignedDataset(BaseDataset):
                 mask2 = mask2.index_select(2, idx)
             mask2 = (mask2 >= 0.5).float()
 
-            hair_A = (A/2+0.5) * mask.repeat(input_nc/output_nc,1,1) * mask2.repeat(input_nc/output_nc,1,1) * 2 - 1
+	    hair_A = (A/2+0.5) * mask.repeat(3,1,1) * mask2.repeat(3,1,1) * 2 - 1
+            #hair_A = (A/2+0.5) * mask.repeat(input_nc/output_nc,1,1) * mask2.repeat(input_nc/output_nc,1,1) * 2 - 1
             hair_B = (B/2+0.5) * mask * mask2 * 2 - 1
-            bg_A = (A/2+0.5) * (torch.ones(mask2.shape)-mask2).repeat(input_nc/output_nc,1,1) * 2 - 1
+ 	    bg_A = (A/2+0.5) * (torch.ones(mask2.shape)-mask2).repeat(3,1,1) * 2 - 1
+            #bg_A = (A/2+0.5) * (torch.ones(mask2.shape)-mask2).repeat(input_nc/output_nc,1,1) * 2 - 1
             bg_B = (B/2+0.5) * (torch.ones(mask2.shape)-mask2) * 2 - 1
             item['hair_A'] = hair_A
             item['hair_B'] = hair_B
